2230b124c59ce3c49a9c46fdba6e7dbd8ca7ef72
Nurmuhammed <sagyndyk0006@gmail.com>
2025-08-03 14:24:41 +0500

    add BearerAuth to Swagger

--- src/main/java/com/example/cinemabooking/config/OpenApiConfig.java
+++ src/main/java/com/example/cinemabooking/config/OpenApiConfig.java
@@ -1,25 +1,31 @@
 package com.example.cinemabooking.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "BearerAuth";
+
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Cinema Booking API")
-                        .version("1.0")
-                        .description("Документация для Cinema Booking проекта")
-                        .contact(new Contact()
-                                .name("Nurmuhammed Sagyndyk")
-                                .email("nsagyndyk006@gmail.com")
-                        )
-                );
+                        .version("1.0.0")
+                        .description("API documentation for Cinema Booking App"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
     }
 }
